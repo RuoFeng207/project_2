eb770028ca3489ad9d198d146c29c4ad3a8bf150
RuoFeng207 <99081157@mydavinci.nl>
2025-01-29 15:07:20 +0100

    Update PDF_2.py

--- PDF_2.py
+++ PDF_2.py
@@ -12,6 +12,13 @@ Title_pagina = "ScaleSkills"
 c = canvas.Canvas(file_name, pagesize=A4)
 width, height = A4
 print(height,width)
+# # afbeelding
+# x_img= 421
+# y_img= 580
+# width = 200  
+# height = 150
+# image_path = "path/to/your/image.jpg"  
+# c.drawImage(image_path, x_img, y_img, width, height)
 # bovenste lijn 
 x3,y3=0,590
 x4,y4=595,590
@@ -24,6 +31,12 @@ c.line (x1,y2,x2,y2)
 x5,y5 =279.5,210
 x6,y6 = 595,210
 c.line(x5,y5,x6,y6)
+
+
+# tekst
+c.drawString(30, 750, "Adres: Europasingel 102693 GW Wervershoof")
+c.drawString(30, 730, "Tel: 06-51944831")
+c.drawString(30, 710, "E mail: SSkills@info.com")
 
 # Toon canvas
 c.showPage()
