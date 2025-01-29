ae4cf9c684250ed33371336472c4bcc47a03eacd
RuoFeng207 <99081157@mydavinci.nl>
2025-01-29 14:43:00 +0100

    update

--- PDF_2.py
+++ PDF_2.py
@@ -12,10 +12,18 @@ Title_pagina = "ScaleSkills"
 c = canvas.Canvas(file_name, pagesize=A4)
 width, height = A4
 print(height,width)
+# bovenste lijn 
+x3,y3=0,590
+x4,y4=595,590
+c.line(x3,y3,x4,y4)
 #middelste lijn 
-x1,x2=0,421
-y1,y2=595,421
-c.line (x1,x2,y1,y2)
+x1,y1=0,421
+x2,y2=595,421
+c.line (x1,y2,x2,y2)
+# onderste lijn 
+x5,y5 =279.5,210
+x6,y6 = 595,210
+c.line(x5,y5,x6,y6)
 
 # Toon canvas
 c.showPage()
