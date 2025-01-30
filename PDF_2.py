c5614f7803120fcd7a7684c7e2e2211f72870e44
RuoFeng207 <99081157@mydavinci.nl>
2025-01-30 11:07:59 +0100

    update

--- PDF_2.py
+++ PDF_2.py
@@ -19,75 +19,100 @@ width = 290
 height = 110
 image_path = "sskills.png"  
 c.drawImage(image_path, x_img, y_img, width, height)
-# bovenste lijn 
-x3,y3=0,565
-x4,y4=595,565
-c.line(x3,y3,x4,y4)
-#middelste lijn 
-x1,y1=0,421
-x2,y2=595,421
-c.line (x1,y2,x2,y2)
-# onderste lijn 
-x5,y5 =279.5,310
-x6,y6 = 595,310
-c.line(x5,y5,x6,y6)
+# Bovenste lijn
+c.line(0, 505, 595, 505) 
 
-# normale tekst grote
+# Middelste lijn 
+c.line(0, 361, 595, 361)  
+
+# Onderste lijn 
+c.line(279.5, 250, 595, 250)  
+
+# driehoek blauw
+# lijn a
+c.line(0, 692, 297, 842)
+# lijn b
+c.line(0, 692, 0, 842)
+# lijn c
+c.line(0, 842, 297, 842) 
+
+#driehoek geel
+# Lijn a 
+c.line(595, 150, 297, 0)  
+
+# Lijn b
+c.line(595, 150, 595, 0) 
+
+# Lijn c 
+c.line(297, 0,595,0)  
 c.setFont("Helvetica", 11)
-# tekst klant
-c.drawString(30, 750, "Adres: Europasingel 102693 GW Wervershoof")
-c.drawString(30, 730, "Tel: 06-51944831")
-c.drawString(30, 710, "E mail: SSkills@info.com")
-#facatuur 
-c.drawString(30,640, "INV-2025-001234")
-c.drawString(145,640, "28 januari 2025")
-c.drawString(250,640, "1 juni 2025")
-#KVK BTW en bank
-c.drawString(30,600, "KVK nummer")
-c.drawString(145,600, "BTW nummer")
-c.drawString(250,600, "Bank nummer") 
-# uren bedrag btw
-c.drawString(30,550, "Datum dienst geleverd - 28 januari-31januari")
-c.drawString(30,530, "Dienst levering - fixen van software problemen")
-c.drawString(270,550, "4 uren")
-c.drawString(335,550, "€100")
-c.drawString(420,550, "21%")
-#klant
-c.drawString(100,400, "Hans visser")
-c.drawString(70,380, "Bloklandweg 4171 KA Herwijnen")
-c.drawString(108,360, "12")
-c.drawString(53,340, "+31 0345 - 12 34 56")
-#Totaal +- btw
-c.drawString(279.5,400, "Totaal exclusief btw (21%) ")
-c.drawString(279.5,380, "Btw €")
+c.setFont("Helvetica", 11)
+# Tekst klant
+c.drawString(30, 690, "Adres: Europasingel 102693 GW Wervershoof")  
+c.drawString(30, 670, "Tel: 06-51944831")  
+c.drawString(30, 650, "E mail: SSkills@info.com")  
 
+# Factuur
+c.drawString(30, 580, "INV-2025-001234")  
+c.drawString(145, 580, "28 januari 2025")  
+c.drawString(250, 580, "1 juni 2025")  
 
+# KVK BTW en bank
+c.drawString(30, 540, "736493") 
+c.drawString(145, 540, "83949484")  
+c.drawString(250, 540, "93047372") 
 
-#Dik gedrukte tekst 
+# Uren bedrag btw
+c.drawString(30, 490, "Datum dienst geleverd - 28 januari-31 januari") 
+c.drawString(30, 470, "Dienst levering - fixen van software problemen")  
+c.drawString(270, 490, "4 uren") 
+c.drawString(335, 490, "€100")  
+c.drawString(420, 490, "21%")  
+
+# Klant
+c.drawString(100, 340, "Hans visser") 
+c.drawString(70, 320, "Bloklandweg 4171 KA Herwijnen")  
+c.drawString(108, 300, "12") 
+c.drawString(53, 280, "+31 0345 - 12 34 56")  
+
+# Totaal +- btw
+c.drawString(279.5, 340, "Totaal exclusief btw (21%) €")  
+c.drawString(279.5, 320, "Btw €") 
+c.drawString(420, 340, "400")  
+c.drawString(310, 320, "84") 
+
+# Inclusief btw
+c.drawString(430, 230, "€")  
+c.drawString(440, 230, "484")  
+
+# Dik gedrukte tekst 
 c.setFont("Helvetica-Bold", 18)
-#tekst facatuur
-c.drawString(30,680, "Factuur")
-c.setFont("Helvetica-Bold", 12)
-c.drawString(30,660, "Factuur nummer")
-c.drawString(145,660, "Factuur datum")
-c.drawString(250,660, "verval datum") 
-#KVK BTW en bank
-c.drawString(30,620, "736493")
-c.drawString(145,620, "83949484")
-c.drawString(250,620, "93047372")
-# uren bedrag btw
-c.drawString(30,570, "Omschrijving")
-c.drawString(270,570, "Uren")
-c.drawString(335,570, "Bedrag")
-c.drawString(420,570, "Btw")
-#klant
-c.drawString(30,400, "Klant naam:")
-c.drawString(30,380, "Adres:")
-c.drawString(30,360, "Huisnummer:")
-c.drawString(30,340, "Tel:")
-#Totaal +- btw
-c.drawString(279.5,290, "Totaal inclusief btw (21%)") 
 
+# Tekst factuur
+c.drawString(30, 620, "Factuur")  
+c.setFont("Helvetica-Bold", 12)
+c.drawString(30, 600, "Factuur nummer")  
+c.drawString(145, 600, "Factuur datum")  
+c.drawString(250, 600, "Verval datum")  
+# KVK BTW en bank
+c.drawString(30, 560, "KVK nummer") 
+c.drawString(145, 560, "BTW nummer")  
+c.drawString(250, 560, "Bank nummer")
+
+# Uren bedrag btw
+c.drawString(30, 510, "Omschrijving")  
+c.drawString(270, 510, "Uren") 
+c.drawString(335, 510, "Bedrag")  
+c.drawString(420, 510, "Btw") 
+
+# Klant
+c.drawString(30, 340, "Klant naam:")  
+c.drawString(30, 320, "Adres:")  
+c.drawString(30, 300, "Huisnummer:") 
+c.drawString(30, 280, "Tel:") 
+
+# Totaal +- btw
+c.drawString(279.5, 230, "Totaal inclusief btw (21%)")  
 
 
 
