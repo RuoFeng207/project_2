4aa408613c6141626c4fb8a843773cb827816ca5
DionKlop <99081201@mydavinci.nl>
2025-01-30 09:55:14 +0100

    Update factuur.py

--- deel 2/factuur.py
+++ deel 2/factuur.py
@@ -18,4 +18,11 @@ product_naam = producten["productnaam"]
 product_aantal = producten["aantal"]
 product_prijs = producten["prijs_per_stuk_excl_btw"]
 product_btw_procent = producten["btw_percentage"]
-
+product_totprijs = product_aantal * product_prijs
+if product_btw_procent == 6:
+    product_btwprijs = ((product_totprijs / 100) * 106) - product_totprijs
+elif product_btw_procent == 9:
+    product_btwprijs = ((product_totprijs / 100) * 109) - product_totprijs
+elif product_btw_procent == 21:
+    product_btwprijs = ((product_totprijs / 100) * 121) - product_totprijs
+print(product_btwprijs)
